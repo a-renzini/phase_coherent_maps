f392d69ca82362d12ce258a100869c6ccbc723e8
Arianna Renzini <arianna.renzini@ldas-grid.ligo.caltech.edu>
2021-02-26 09:45:21 -0800

    up

---
+++ src_code/match_phase.py
@@ -0,0 +1,12 @@
+CONFIG_PARAMS = {'nside_in': 16,
+                            'nside_out': 16,
+                            'dect_labels': ['H1','L1'], # 'V1', 'K'],
+                            'tag': 'match_phase',
+                            'map_gen_flag': False,
+			    'input_dir': '/home/arianna.renzini/PROJECTS/phase_coherent_maps/GB_point_source',
+			    'output_path': '/home/arianna.renzini/PROJECTS/phase_coherent_maps/output_maps',
+                            'N_freqs': 200,
+			    'freq_min': 80,
+			    'freq_max': 300}
+
+
